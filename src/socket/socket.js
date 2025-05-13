779d3fb12c8048592391ded8d42a04b2d199ac76
HAJPRO <texhaj7@gmail.com>
2025-05-12 16:37:02 -0800

    Yangi o‘zgarish

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // Agar token kerak bo'lsa, uni avtomatik qo'shamiz
 const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 const URL = "https://eco.company-erp.uz"; // Asosiy URL
-// const URL = "http://localhost:3000";// // localhostda ishlaganda bu qatorni oching
+// const URL = "http://localhost:3000";// // localhostda ishlaganda bu qatorni oching/////
 const socket = io(URL, {
     autoConnect: false, // ⚠️ Auto connectni o‘chirib qo‘yamiz, qo‘lda ulaymiz!
     reconnection: true, // Uzilib qolsa, qayta ulanadi
